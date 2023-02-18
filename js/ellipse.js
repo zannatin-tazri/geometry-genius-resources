47a3c1ea9ece67be3bf9bc7a9d6548fa85800e41
zannatin-tazri <zannatintazrizan@gmail.com>
2023-02-18 19:46:58 +0600

    Calculated Ellipse

---
+++ js/ellipse.js
@@ -0,0 +1,10 @@
+// // add eventlistener
+document.getElementById('btn-ellipse-calculation').addEventListener('click',function(){
+    const a=10;
+    const b=4;
+    const areaOfEllipse=3.14*10*4;
+
+    // setting innerText
+    const calculationEllipseBtn=document.getElementById('calculated-ellipse');
+    calculationEllipseBtn.innerText=areaOfEllipse;
+})
